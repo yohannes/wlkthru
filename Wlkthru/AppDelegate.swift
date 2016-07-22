d37d28dbacc4e5558133f1376bf5e92c4f1b32dc
Yohannes Wijaya <readyforservice@icloud.com>
2016-07-22 16:09:13 +0700

    Modified the page indicators so they're visible on screen.

--- Wlkthru/AppDelegate.swift
+++ Wlkthru/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        let pageControl = UIPageControl.appearance()
+        pageControl.currentPageIndicatorTintColor = UIColor.blackColor()
+        pageControl.pageIndicatorTintColor = UIColor.lightGrayColor()
+        pageControl.backgroundColor = UIColor.whiteColor()
+        
         return true
     }
 
