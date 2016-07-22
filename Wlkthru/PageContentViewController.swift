d4dc2e0abc565a1aa2b159b38c4a179e6e4319db
Yohannes Wijaya <readyforservice@icloud.com>
2016-07-22 13:07:41 +0700

    Moved the Register & Login UIButton to the view of RootVC instead of the view of PageContentVC. Create PageContentVC class subclassed from UIViewController. Created and connected all the IBOutlet properties and IBAction methods.

---
+++ Wlkthru/PageContentViewController.swift
@@ -0,0 +1,27 @@
+//
+//  PageContentViewController.swift
+//  Wlkthru
+//
+//  Created by Yohannes Wijaya on 7/22/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class PageContentViewController: UIViewController {
+    
+    // MARK: - IBOutlet Properties
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var subheaderLabel: UILabel!
+    
+    // MARK: - UIViewController Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Wlkthru/RootViewController.swift
+++ Wlkthru/RootViewController.swift
@@ -9,17 +9,22 @@
 import UIKit
 
 class RootViewController: UIViewController {
+    
+    // MARK: - IBAction Methods
+    
+    @IBAction func registerButtonDidTouch(sender: UIButton) {
+        
+    }
+    
+    @IBAction func logInButtonDidTouch(sender: UIButton) {
+        
+    }
+    
+    // MARK: - UIViewController Methods
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
