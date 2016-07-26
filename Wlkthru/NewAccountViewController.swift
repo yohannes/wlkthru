b3179ca50839368852741c2680d4c5b75ba4d352
Yohannes Wijaya <readyforservice@icloud.com>
2016-07-26 15:45:55 +0700

    Setup the controller, logic, and presentation for Register button

---
+++ Wlkthru/NewAccountViewController.swift
@@ -0,0 +1,36 @@
+//
+//  NewAccountViewController.swift
+//  Wlkthru
+//
+//  Created by Yohannes Wijaya on 7/26/16.
+//  Copyright © 2016 Yohannes Wijaya. All rights reserved.
+//
+
+import UIKit
+
+class NewAccountViewController: UIViewController {
+    
+    // MARK: - IBOutlet Methods
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var passwordConfirmationTextField: UITextField!
+    
+    // MARK: - IBAction Methods
+    
+    @IBAction func cancelButtonDidTouch(sender: UIBarButtonItem) {
+        self.emailTextField.resignFirstResponder()
+        self.passwordTextField.resignFirstResponder()
+        self.passwordConfirmationTextField.resignFirstResponder()
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    // MARK: - UIViewController Methods
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.emailTextField.becomeFirstResponder()
+    }
+
+}
